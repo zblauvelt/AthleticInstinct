0fc1ccb8f5219a0e1757d7bdfbf5578ef8c5031e
zblauvelt <zblauvelt@hotmail.com>
2017-09-25 21:51:54 -0400

    User Interface
    Added Logo, tagline, and background image

--- AthleticInstinct/AppDelegate.swift
+++ AthleticInstinct/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FIRApp.configure()
+        //Change time content color
+        UIApplication.shared.statusBarStyle = .lightContent
+
+        
         return true
     }
 
